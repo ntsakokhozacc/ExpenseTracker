dd8ffd8c311a1b314235bf88a877acde1afd175a
ntsako <ntsakokhozacc@gmail.com>
2021-12-07 00:51:42 +0200

    ft-clear text files after submitting values

--- script.js
+++ script.js
@@ -1,71 +1,79 @@
 let detail;
 let amount;
 
-//holds all transactions
+// holds all transactions
 let transactions = [];
 
-//get values from inputs
-function getInputValues(){
-    detail=document.getElementById('detail').value;
-    amount=Number(document.getElementById('amount').value);
+// get values from inputs
+function getInputValues() {
+  detail = document.getElementById("detail").value;
+  amount = Number(document.getElementById("amount").value);
 }
 
-//validates the inputs
-function isValid(){
-    if(!detail || !amount){
-        alert("Detail and amount are both required");
-        return false;
-    }   
-    return true;
+// validate inputs
+function isValid() {
+  if (!detail || !amount) {
+    alert("Detail and Amount are both required");
+    return false;
+  }
+  return true;
 }
 
-//populate the table
-function displayTable(){
-    const tableBody = document.getElementById("tableBody");
+// populate the table
+function displayTable() {
+  const tableBody = document.getElementById("tableBody");
 
-    tableBody.innerHTML = "";
+  tableBody.innerHTML = "";
 
-    for(let i=0; i<transactions.length; i++){
-        tableBody.innerHTML += `
+  for (let i = 0; i < transactions.length; i++) {
+    tableBody.innerHTML += `
         <tr>
                 <th>${transactions[i].type}</th>
                 <th>${transactions[i].detail}</th>
                 <th>${transactions[i].amount}</th>
             </tr>
         `;
-    }
+  }
 }
 
-function getResults(){
+
+function getResults () {
     const incomeTotal = document.getElementById("incomeTotal");
     const expenseTotal = document.getElementById("expenseTotal");
-    const amountTotal =  document.getElementById("amountTotal");
+    const amountTotal = document.getElementById("amountTotal");
 
     let income = 0;
-    let expense = 0
-    let amount =0;
+    let expense = 0;
+    let amount = 0;
 
-    if (transactions[i].type ==="Income"){
-        income=income+transactions[i].amount;
+   for (let i = 0; i < transactions.length; i++) {
+
+    if (transactions[i].type === "Income") {
+        income += transactions[i].amount;
     }
-    if(transactions[i].type === "Expense"){
-        expense=expense+transactions[i].amount;
-    } 
 
-    incomeTotal.innerHTML = income;
-    expenseTotal.innerHTML = expense;
-    amountTotal.innerHTML = income - expense;
-}
+    if (transactions[i].type === "Expense") {
+        expense += transactions[i].amount;
+    }
+       
+   }
 
-function calc(type){
-    getInputValues();
-    
-    if(!isValid()) return;
-
-    transactions.push({type,detail,amount})
-    getResults();
-    displayTable();
+   incomeTotal.innerHTML = income;
+   expenseTotal.innerHTML = expense;
+   amountTotal.innerHTML = income - expense;
 }
 
 
+function calc(type) {
+  getInputValues();
+
+  if (!isValid()) return;
+
+  transactions.push({ type, detail, amount })
+  getResults();
+  displayTable();
+  document.getElementById("detail").value="";
+  Number(document.getElementById("amount").value=null);
+
+}
 
