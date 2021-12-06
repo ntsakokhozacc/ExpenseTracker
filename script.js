f64143d26ac127802bd5b49faee47cf9925f64b1
ntsako <ntsakokhozacc@gmail.com>
2021-12-06 23:52:09 +0200

    stying and scripts

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let detail;
 let amount;
 
+//holds all transactions
+let transactions = [];
+
+//get values from inputs
 function getInputValues(){
     detail=document.getElementById('detail').value;
     amount=Number(document.getElementById('amount').value);
-
 }
 
+//validates the inputs
 function isValid(){
     if(!detail || !amount){
         alert("Detail and amount are both required");
@@ -15,18 +19,53 @@ function isValid(){
     return true;
 }
 
-function add_income(){
-    console.log("income")
-}
+//populate the table
+function displayTable(){
+    const tableBody = document.getElementById("tableBody");
 
-function calcExpense(){
-    console.log("expense");
-}
+    tableBody.innerHTML = "";
 
-function calcExpense(){
-    getInputValues();
-    if(!isValid()){
-        return false;
+    for(let i=0; i<transactions.length; i++){
+        tableBody.innerHTML += `
+        <tr>
+                <th>${transactions[i].type}</th>
+                <th>${transactions[i].detail}</th>
+                <th>${transactions[i].amount}</th>
+            </tr>
+        `;
     }
-    console.log(detail,amount);
 }
+
+function getResults(){
+    const incomeTotal = document.getElementById("incomeTotal");
+    const expenseTotal = document.getElementById("expenseTotal");
+    const amountTotal =  document.getElementById("amountTotal");
+
+    let income = 0;
+    let expense = 0
+    let amount =0;
+
+    if (transactions[i].type ==="Income"){
+        income=income+transactions[i].amount;
+    }
+    if(transactions[i].type === "Expense"){
+        expense=expense+transactions[i].amount;
+    } 
+
+    incomeTotal.innerHTML = income;
+    expenseTotal.innerHTML = expense;
+    amountTotal.innerHTML = income - expense;
+}
+
+function calc(type){
+    getInputValues();
+    
+    if(!isValid()) return;
+
+    transactions.push({type,detail,amount})
+    getResults();
+    displayTable();
+}
+
+
+
